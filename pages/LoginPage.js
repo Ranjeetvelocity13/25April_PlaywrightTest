6dd22e2e26d8dafa3172e240a20324501b6d9fa1
Ranjeetvelocity13 <ranjeetvelocity13@gmail.com>
2026-08-05 08:15:33 +0530

    Updated Login Page class

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -26,6 +26,7 @@ class LoginPage extends BasePage {
     // Complete the login flow by entering credentials and submitting.
     async login(username, passowrd) {
         Logger.step(2, 'Performing login flow');
+        Logger.step(3, 'Performing  DashBoard flow');
 
         try {
             await this.fill(this.usernameInput, username);
